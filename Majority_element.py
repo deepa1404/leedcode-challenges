72b62ee88445e71404326ea54931b69198ac52b8
Deepa KR <deepasujatha19@gmail.com>
2026-07-29 19:18:09 +0530

    Add files via upload

---
+++ Majority_element.py
@@ -0,0 +1,16 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        can = 0
+        for num in nums:
+            if count == 0:
+                can = num
+            if can == num:
+                count +=1
+            else:
+                count -= 1
+
+        return can
+
+
+            
